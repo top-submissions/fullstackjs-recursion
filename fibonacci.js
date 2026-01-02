16f1297ee66e03cda8a11225b54a21b1e9daa100
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-02 20:23:35 +0800

    feat: add fibsRec function

--- fibonacci.js
+++ fibonacci.js
@@ -17,3 +17,25 @@ function fibs(n) {
 
   return result;
 }
+
+/**
+ * Generate Fibonacci sequence using recursion
+ * @param {number} n - Number of Fibonacci numbers to generate
+ * @param {number[]} sequence - Accumulator array (for internal use)
+ * @returns {number[]} Array containing n Fibonacci numbers
+ */
+function fibsRec(n, sequence = [0, 1]) {
+  // Uncomment line below to visualize recursive calls
+  // console.log("This was printed recursively");
+
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  if (sequence.length >= n) return sequence;
+
+  const len = sequence.length;
+  sequence.push(sequence[len - 1] + sequence[len - 2]);
+
+  return fibsRec(n, sequence);
+}
+
+export { fibs, fibsRec };
